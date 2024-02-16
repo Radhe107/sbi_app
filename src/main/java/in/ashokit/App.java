c61554d9c45d5cd28d65c72982aa8fdd9ea2c72a
Radhe107 <86216139+Radhe107@users.noreply.github.com>
2024-02-16 15:06:35 +0530

    Update App.java

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! this USS marine" );
+        System.out.println( "Hello World! this USS marine and USSR Army" );
     }
 }
